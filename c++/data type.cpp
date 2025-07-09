37a854266858c4b3ea74074ac80892055b991b6f
tsion <tsionyeshetila977@gmail.com>
2025-07-09 03:52:27 -0700

    c++ data types

---
+++ c++/data type.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+ 
+ int main(){
+ int age;
+ age =20;
+ string name = "Tsion";
+ 
+ cout<<"my age is: "<<age<<endl;
+ cout<<"my name is: "<<name<<endl;	
+ 	
+ return 1;
+ }
