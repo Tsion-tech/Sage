1078dc72f9441b2a29ce639f0abeecca31869dfd
tsion <tsionyeshetila977@gmail.com>
2025-07-10 04:10:14 -0700

    comparing

---
+++ c++/comparing.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	int Fn;
+	int sn;
+	
+	cout<<"enter Fn :";
+	cin>>Fn;
+		cout<<"enter sn :";
+		cin>>sn;
+	
+	if(Fn>sn){
+		
+		cout<<Fn<<endl;
+	}
+	
+	else{
+		
+		cout<<sn<<endl;
+	}
+	
+	
+		return 0;
+}
