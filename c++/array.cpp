e4dbc2c6dfc5952f4eb0adc84e68b63d8d7c523c
tsion <tsionyeshetila977@gmail.com>
2025-07-10 14:29:48 -0700

    array

---
+++ c++/array.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	
+int num[]={1,2,3,4,5,6,7,8,9,11};
+num[3]=10;
+
+for(int i=0;i<=9;i++){
+	cout<<num[i]<<endl;
+	
+}	
+	
+	
+	
+	return 0;
+}
