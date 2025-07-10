b5078b1f46578de51bdeb12c9b60f0364abb6027
tsion <tsionyeshetila977@gmail.com>
2025-07-10 13:55:07 -0700

    fahrenheit

---
+++ c++/fahrenheit.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	
+	
+	double celcius;
+const double value=1.8;
+const double fah=32;
+float fahrenheit;
+ 
+ cout<<"enter"<<endl;
+ cin>>celcius;
+ 
+ 
+ fahrenheit=(celcius*1.8)+32;
+ cout<<"fahrenheit"<<fahrenheit<<endl;
+ 
+ 	return 0;
+}
