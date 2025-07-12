b6317e73a262b97d3b9c2f1897089b91b70584cd
Tsion-yeshetila <tsionyeshetila977@gmail.com>
2025-07-11 22:35:03 -0700

    sum

---
+++ sum/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+    int x;
+    int y;
+    int sum;
+    cout<<"Enter the first number :"<<endl;
+    cin>>x;
+    cout<<"Enter the second number :"<<endl;
+    cin>>y;
+
+    sum=x+y;
+    cout << "The Sum is :"<<sum<< endl;
+    return 0;
+}
