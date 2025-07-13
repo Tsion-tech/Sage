320ab4a0dce9a2a9795f538c6c729f8a77ac4f9b
Tsion-yeshetila <tsionyeshetila977@gmail.com>
2025-07-12 19:28:01 -0700

    circumstance added

---
+++ c++/circumstance.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+double r;
+const double pi = 3.145;
+double c;
+
+cout<<"Enter the value of radius :"<<endl;
+cin>>r;
+
+c =2*pi*r;
+cout<<"circumstance = "<<c<<endl;
+
+	
+	
+	
+	
+		
+	return 0;
+}
