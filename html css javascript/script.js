3fe55de5f53cb79466a369f15d6d239b31d6c06a
tsion <tsionyeshetila977@gmail.com>
2025-07-13 02:52:52 -0700

    website practice

--- html css javascript/script.js
+++ html css javascript/script.js
@@ -3,5 +3,28 @@ alert('hi Js')
 
 }
 function add(){
-    let x=1
+    let x=1;
+    let y=3;
+    y=7;
+    alert(x+y)
 }
+let x=1;
+let y=3;
+let isjavascript =true;
+let array =["A","B","C"];
+let person={name:"Rahel",age:22,gender:"Female"};
+console.log("addition: ",x+y);
+console.log("boolean: ",isjavascript);
+console.log("Array: ",array[2]);
+console.log("person name: ",person.name);
+
+let age=17;
+if(age>=21){
+    console.log("Adult");
+}else if(age<21&&age>14){
+console.log("Teen");
+}else{
+    console.log("kid");
+}
+
+
