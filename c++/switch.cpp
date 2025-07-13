501be75a1b2721eac0d29f55c0d823bd3babec3a
Tsion-yeshetila <tsionyeshetila977@gmail.com>
2025-07-12 19:55:27 -0700

    switch

---
+++ c++/switch.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+int day;
+cout<<"Enter day :"<<endl;
+cin>>day;
+
+switch(day){
+	
+	case 1: 
+	cout<<"monday";
+	break;
+	
+	
+case 2: 
+	cout<<"tuesday";
+	break;
+	
+	case 3: 
+	cout<<"wednesday";
+	break;
+	
+	default: cout<<"unknown day";	
+	
+}
+
+
+
+	
+	return 0;
+}
