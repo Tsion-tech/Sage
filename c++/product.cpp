030acc4694751c1f245a58b7712a894da51c1f40
Tsion-yeshetila <tsionyeshetila977@gmail.com>
2025-07-12 19:21:26 -0700

    product opration

---
+++ c++/product.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	
+	int x;
+	int y;
+	int product;
+	cout<<"Enter the first number :"<<endl;
+	cin>>x;
+	cout<<"Enter the second number :"<<endl;
+	cin>>y;
+	
+	product=x*y;
+	
+	cout<<"the product is :"<<product<<endl;
+	
+	
+	
+	
+	return 0;
+}
