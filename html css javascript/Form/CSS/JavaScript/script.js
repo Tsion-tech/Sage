520027bde44d9190339a53629f10fd6c3e1eb85b
tsion <tsionyeshetila977@gmail.com>
2025-07-15 00:23:36 -0700

    JQUERY

--- html css javascript/Form/CSS/JavaScript/script.js
+++ html css javascript/Form/CSS/JavaScript/script.js
@@ -1,11 +1,53 @@
 let bulb="off";
+const newDiv = document.createElement("h1")
+
+// document.getElementById("btClick").addEventListener("click",insertHTML)
+$("#btClick").click(insertHTML)
+
 function insertHTML(){
-    document.getElementById("chengMe").style.fontSize=""
-    document.getElementById("chengMe").style.color=""
+    document.getElementById("chengMe").style.fontSize="35px"
+    document.getElementById("chengMe").style.color="red"
+ 
+    // document.getElementById("chengMe").style.display="none"
+    
+    newDiv.innerText="added element"
+    document.body.appendChild(newDiv);
 }
 function turnon(){
-    document.getElementById("bulb").style.fontSize=""
+    document.getElementById("bulb").src="pic_bulbon.gif"
+    buld="on";
 }
+    
+    function turnoff(){
+    document.getElementById("bulb").src="pic_bulboff.gif"
+    buld="off";
+    
+
+}
+function toggle(){
+    if(bulb=="on"){
+        document.getElementById("bulb").src="pic_bulboff.gif"
+        bulb="off"
+    }else{
+       document.getElementById("bulb").src="pic_bulbon.gif"
+        bulb="on"; 
+    }
+}
+
+
+ $('#box').slideUp(2000).slideDown(2000) .css("background-color","brown").css("width","100px").css("height","100px")
+//  $('#box').css("width","100px");
+//  $('#box').css("height","100px");
+
+// $('#box').css("background-color","brown")
+//  .css("width","400px")
+//  .css("height","100px")
+//  .css("justify-content","center")
+//  .css("font-size","10px")
+//  .css("margin","30px")
+ 
+ 
+
 
 const person={
     firstName:"Tsion",
@@ -59,5 +101,46 @@ console.log(text);
 let value
 (4>5)?value=true:value=false
 console.log("Ternary:",value??"the value of value is undefined!")
+let day=1
+switch(day){
+    case 1:
+        console.log("Monday")
+        break;
+         case 2:
+        console.log("Tuesday")
+        break;
+         case 3:
+        console.log("Wednesday")
+        break;
+         case 4:
+        console.log("Thursday")
+        break;
+         case 5:
+        console.log("Friday")
+        break;
+         case 6:
+        console.log("Saturday")
+        break;
+         case 7:
+        console.log("Sunday")
+        break;
+}
+let i=0
+while(i<3){
+    console.log(i);
+    i++;
+}
+for(let i=0;i<5;i++) {
+    console.log(i);
+}
 
+let books=["herry poter","love up to death","me and u","the power","think and grow rich"]
+for(let book of books){
+
+    console.log(book)
+}
+
+for(let key in person){
+    console.log(key,person[key])
+}
 
