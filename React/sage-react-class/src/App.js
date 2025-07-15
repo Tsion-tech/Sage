1cceb83fcc12fbbb43d89f0099403b75636ff795
tsion <tsionyeshetila977@gmail.com>
2025-07-15 13:09:08 -0700

    react

---
+++ React/sage-react-class/src/App.js
@@ -0,0 +1,12 @@
+
+import Button from "./components/Button"
+import"./App.css"
+function App(){
+  return <div>
+    <h1>First component</h1>
+    <Button text="Login"/>
+    <br/><br/>
+    <Button text="Register"/>
+    </div>
+}
+export default App
---
+++ React/sage-react-class/src/components/Button.jsx
@@ -0,0 +1,4 @@
+function Button(props){
+    return<a className="btn"href="#">{props.text}</a>
+}
+export default Button;
