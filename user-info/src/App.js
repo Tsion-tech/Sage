609fd1e25f0b9982d380ea2aef017e7a5ece0bea
Tsion-yeshetila <tsionyeshetila977@gmail.com>
2025-08-06 00:51:50 -0700

    React userinfo

---
+++ user-info/src/App.js
@@ -0,0 +1,25 @@
+import { useState, useEffect } from "react";
+
+function App() {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then((res) => res.json())
+      .then((data) => setUsers(data));
+  }, []);
+
+  return (
+    <div>
+      {users.map((user, index) => (
+        <div key={index}>
+          <h1>{user.name}</h1>
+          <h2>{user.email}</h2>
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default App;
+ 
