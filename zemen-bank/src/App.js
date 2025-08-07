c87adce6b47c216885f3d0292bb96a9f9cbadc71
Tsion-yeshetila <tsionyeshetila977@gmail.com>
2025-08-07 04:49:16 -0700

    zemen-bank

--- zemen-bank/src/App.js
+++ zemen-bank/src/App.js
@@ -5,7 +5,7 @@ import Loginpage from './Pages/Loginpage';
 function App() {
   return (
     <div >
-      <h1 style={{color:"black",backgroundColor:"red"}}>Zemen bank</h1>
+      <h1 style={{color:"black",backgroundColor:"red",margin:"auto"}}>Zemen bank</h1>
       <Loginpage/>
     </div>
   );
--- zemen-bank/src/Pages/Loginpage.jsx
+++ zemen-bank/src/Pages/Loginpage.jsx
@@ -1,26 +1,49 @@
-import { useState } from "react"
+import { useState } from "react";
+function LogInPage(){
+    const [userName,setUserName]= useState(" ")
+    const [firstName,setFirstName]= useState(" ")
+    const [lastName,setlasttName]= useState(" ")
+  function handelSubmit(e){
+e.preventDefault()
+console.log("user name:",userName)
+alert("user name: "+ userName)
+  }
+//   const handelSubmit2()=>{
 
-function Loginpage(){
-const[userName,setUserName]=useState("Rahel")
-const[Name,setUserName]
-    return<div>
-        <form>
-            <label htmlFor="userName">UserName</label>
+//   }
+return(
+
+    <div>
+        <h1>Loin Page</h1>
+        <form onSubmit={handelSubmit}>
+            <div>
+                <lable  htmlFor="firstName">First Name</lable>
+                <input type="text" id="firstName" placeholder="Enter your first name" name="firstName" required
+                onChange={(e)=>{
+                    setFirstName(e.target.value)
+                }}/>
+                <h2>firstName: {firstName}</h2>
+                <br/>
+                <lable  htmlFor="lastName">Last Name</lable>
+                <input type="text" id="lastName" placeholder="Enter your last name" name="lasttName" required
+                onChange={(e)=>{
+                    setlasttName(e.target.value)
+                }}/>
+                <h2>lastName: {lastName}</h2>
+                <br/>
+            <label htmlFor="userName">User Name</label>
             <br/>
-            <input
-             type="text" 
-             id="userName" 
-             placeholder="Enter user name"
-              name="user name" 
-              required
-            onChange={
-                (e)=>{
-                    setUserName(e.target.value)
-                }
+            <input type="text" id="username" placeholder="Enter user name" name="username" required
+             onChange={(e)=>{
+                setUserName(e.target.value)
+            }
             }
             />
+            <h1>userName: {userName}</h1>
+            <button typeof="submit">Submit</button>
+            </div>
         </form>
-        
     </div>
+);
 }
-export default Loginpage
+export default LogInPage;
