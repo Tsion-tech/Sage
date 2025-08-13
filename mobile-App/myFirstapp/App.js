8da7c09385df64b9fd4bfd2b9451d5a7487e410f
Tsion-yeshetila <tsionyeshetila977@gmail.com>
2025-08-13 01:54:15 -0700

    my first react-native app

--- mobile-App/myFirstapp/App.js
+++ mobile-App/myFirstapp/App.js
@@ -1,32 +1,36 @@
 
-import {  Text, View,Image, Pressable, } from 'react-native';
+import { useState } from 'react';
+import {  Text, View,Image, Pressable,Switch,SafeAreaView } from 'react-native';
 import { TextInput ,Button} from 'react-native';
 
 export default function App() {
+  const [on,setOn]=useState(false)
   return (
+    <SafeAreaView style={{flex:1}}>
     <View style={{flex:1,justifyContent:"center",alignItems:"center"}}>
       
     <Image source={{uri:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRf6zoRR_FPG7f2knECoYTgOuETejMYPg71vg&s"}} style={{width:385,height:200}}/>
-    <br/> <br/>
-    <Text style={{ fontWeight:"bold", margin:20,padding:"10px",fontSize:30,color:"purple"}}> My first  react-native app ! </Text>
+    
+    <Text style={{ fontWeight:"bold", margin:20,padding:"10px",fontSize:30,color:"purple"}}> My first  react app !! </Text>
     <Image source={require("./assets/favicon.png")}
-      style={{width:70,height:70}}  />
-      <br/> <br/>
+      style={{width:70,height:70,margin:10}}  />
+      
       
       <TextInput
       placeholder="Enter Name"
-      style={{borderWidth:1,padding:10}}/>
-      <br/> <br/>
+      style={{borderWidth:1,padding:10,margin:10}}/>
+      
 
       <Button title="Click me" onPress={()=>alert("Clicked")}
         style={{margin:10}}/>
-        <br/> <br/>
-      <Pressable onPress={()=> alert("Pressable")}>
+        
+      <Pressable style={{margin:10}} onPress={()=> alert("Pressable")}>
         <Text>click me</Text>
       </Pressable>
-      <Swich value={on} onValueChange={(value)=>setOn(value)} />
+      <Switch value={on} onValueChange={(value)=>setOn(value)} />
   
     </View>
+    </SafeAreaView>
   );
 }
 
